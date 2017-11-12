a0176918e62dd5b646f8dd5d4691b2884b2fe29a
marls628 <marlyshawley@outlook.com>
2017-11-12 17:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string){
   return string.toLowerCase()
 }
-function logShout(hello) {
-  console.log(hello).touppercase()
+function logShout('hello') {
+  console.log('hello').touppercase()
 }
