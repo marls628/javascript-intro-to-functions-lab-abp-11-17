80968fddb615f1bd9e68262ec29678d0c7f1db62
marls628 <marlyshawley@outlook.com>
2017-11-12 18:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
   return "I can't hear you!";
   }
+  if (string === string.toUppercase()) {
+    return "YES INDEED!"
+  }
 }
