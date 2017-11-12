f4e8b2b6259c29f9feb3a2021dbeacd252376209
marls628 <marlyshawley@outlook.com>
2017-11-12 18:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
+  if (string.toLowerCase() === string) {
+  return "I can't hear you!"
 }
